c0d7a928b3d5c1140a76881f52b4d6fdbdcbe8cd
Pranav Pagote <pranav1990.pagote@gmail.com>
2021-07-01 16:28:02 +0100

    changes

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Table from './Table'
+import Form from './Form';
+
+class App extends Component {
+/*   constructor(props) {
+    super(props); */
+    state = {
+      characters: [ ],
+    };
+//  }
+  render() {
+    return (
+      <div className="container">
+        <Table characterData={this.state.characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit}/>
+      </div>
+    )
+  }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character] })
+  }
+
+}
+
+export default App
